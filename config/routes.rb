071afe6fdd4117010cf04e241480427aa70ca901
vernj123 <vjacques01@gmail.com>
2014-01-28 19:18:11 -0500

    incidental

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Taxdoc1::Application.routes.draw do
   get "static_pages/home"
   get "static_pages/about"
-  # The priority is based upon order of creation: first created -> highest priority.
+    # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
